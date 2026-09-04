132de4774f76d1171d444c250e4f76c0f69eb3f2
Lunar <lunarlifeburst@gmail.com>
2026-09-04 15:03:37 +1000

    variable font sizing

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
+local fontSize = 32
+
+
+
+local cachedFontSize = 0 -- do not set manually
+
 function love.update(dt)
 end
 
 function love.load()
-    love.graphics.setFont(love.graphics.newFont(32))
 end
 
 function love.draw()
+    if (cachedFontSize ~= fontSize) then
+        love.graphics.setFont(love.graphics.newFont(fontSize))
+        cachedFontSize = fontSize
+    end
     love.graphics.print("Hello", 50, 50)
 end
